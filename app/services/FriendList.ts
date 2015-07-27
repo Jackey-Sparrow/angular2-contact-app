306bfcfb0235110dd535337a7684fa3e644f702f
Jackey <1109952026@qq.com>
2015-07-27 20:47:34 +0800

    basic CRUD practise

--- app/services/FriendList.ts
+++ app/services/FriendList.ts
@@ -1,6 +1,7 @@
 export class FriendList {
     Friends = [
         {
+            id: 1,
             firstName: 'Jackey1',
             lastName: 'Li',
             age: 25
@@ -13,5 +14,27 @@ export class FriendList {
 
     add(value:Object) {
         this.Friends.push(value);
+    }
+
+    getNextFriendId() {
+        var result = 1, len = this.Friends.length;
+        if (!len) {
+            return result;
+        }
+        for (var i = 0; i < len; i++) {
+            var friend = this.Friends[i];
+            if (friend.id > result) {
+                result = friend.id;
+            }
+        }
+        return result + 1;
+    }
+
+    delete(friend) {
+        var len = this.Friends.length,
+            index = this.Friends.indexOf(friend);
+        if (index !== -1) {
+            this.Friends.splice(index, 1);
+        }
     }
 }
